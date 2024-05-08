ced9247d9ac16a2914d03d638afc34e78d650250
ridazaneb <157319768+ridazaneb@users.noreply.github.com>
2024-05-07 23:18:06 -0400

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
   resources :workouts
+  resources :exercises
+  resources :login
+  resources :signup
   # Defines the root path route ("/")
   # root "posts#index"
 end
