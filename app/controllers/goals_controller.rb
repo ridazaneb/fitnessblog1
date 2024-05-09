aac1cc3c1da196f6c9e09a34b08542e88c702cfa
ridazaneb <157319768+ridazaneb@users.noreply.github.com>
2024-05-09 09:39:28 -0400

    Update goals_controller.rb

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -17,4 +17,13 @@ class GoalsController < ApplicationController
   def new
     @goal = Goal.new
   end
+  def create
+  @goal = Goal.new(goal_params)
+  if @goal.save
+    redirect_to goals_path, notice: "Goal was successfully created."
+  else
+    render :new
+  end
+end
+
 end
