b627fffff065cfb029aad5dc90f0d36474607447
ridazaneb <157319768+ridazaneb@users.noreply.github.com>
2024-05-09 08:55:13 -0400

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user
-
+  
+  def authenticate_user!
+    unless current_user
+      redirect_to login_path, alert: "Please log in to continue"
+    end
+  end
+  
   def current_user
     @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
   end
