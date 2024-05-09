2a15049988a7c1bc8b4d78f28f9954d3e01d1d41
ridazaneb <157319768+ridazaneb@users.noreply.github.com>
2024-05-09 08:58:35 -0400

    Update reviews_controller.rb

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,6 @@
 class ReviewsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
-  before_action :set_review, only: [:edit, :update, :destroy]
+ /* before_action :authenticate_user!, only: [:new, :create] */
+ /* before_action :set_review, only: [:edit, :update, :destroy] */
 
   def new
     @review = Review.new
