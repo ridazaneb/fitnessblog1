c70b53f86e03dd4e5a8cd72de0fa157032881c90
ridazaneb <157319768+ridazaneb@users.noreply.github.com>
2024-05-09 01:22:05 -0400

    Update sessions_controller.rb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
   def new
   end
-end
+
   def create
     @user = User.find_by(username: params[:username])
     if @user && @user.authenticate(params[:password])
@@ -12,3 +12,4 @@ end
       render :new
     end
   end
+end
