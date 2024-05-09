14310509efce4c4b9c18a7a4ce6d29211ca09387
ridazaneb <157319768+ridazaneb@users.noreply.github.com>
2024-05-09 01:14:08 -0400

    Update sessions_controller.rb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,3 +2,13 @@ class SessionsController < ApplicationController
   def new
   end
 end
+  def create
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to root_path, notice: "Welcome, #{@user.username}"
+    else
+      flash.now[:alert] = "Invalid username or password"
+      render :new
+    end
+  end
