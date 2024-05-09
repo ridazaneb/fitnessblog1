83dd07c2d17596717e3e9f88d0b383785a792672
ridazaneb <157319768+ridazaneb@users.noreply.github.com>
2024-05-09 01:04:43 -0400

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,17 @@ class UsersController < ApplicationController
   end
 
   def create
+     @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path, notice: "Sign up successful!"
+    else
+      render :ne
+  end
+end
+
+ private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
   end
 end
