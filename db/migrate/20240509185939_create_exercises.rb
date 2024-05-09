6327d0aa7f50a9dec0defc5c898b259db2e86e48
ridazaneb <157319768+ridazaneb@users.noreply.github.com>
2024-05-09 15:01:23 -0400

    Update 20240509185939_create_exercises.rb

--- db/migrate/20240509185939_create_exercises.rb
+++ db/migrate/20240509185939_create_exercises.rb
@@ -1,8 +1,10 @@
-class CreateExercises < ActiveRecord::Migration[7.1]
+class CreateExercises < ActiveRecord::Migration[6.0]
   def change
     create_table :exercises do |t|
       t.string :name
-
+      t.integer :time
+      t.float :speed
+      t.integer :calories
       t.timestamps
     end
   end
