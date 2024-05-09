d7a5bc9f839aba7ca628b3e2ab9b5e047afb2463
ridazaneb <157319768+ridazaneb@users.noreply.github.com>
2024-05-09 16:05:23 -0400

    Update exercises_controller.rb

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,7 +1,7 @@
 class ExercisesController < ApplicationController
   def index
     @exercises = Exercise.all
-      @exercise_records = current_user.exercise_records.includes(:exercise)
+    @exercise_records = current_user.exercise_records.includes(:exercise) if current_user
 
   end
 end
