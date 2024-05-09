7aed75687af1d631af77525c31ef0796c6b99584
ridazaneb <157319768+ridazaneb@users.noreply.github.com>
2024-05-09 08:54:03 -0400

    Update reviews_controller.rb

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,13 +1,14 @@
 class ReviewsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_review, only: [:show, :edit, :update, :destroy]
 
   # GET /reviews/new
+  before_action :authenticate_user!, only: [:new, :create]
+
+
   def new
     @review = Review.new
   end
 
-  # POST /reviews
   def create
     @review = current_user.reviews.build(review_params)
     if @review.save
@@ -19,12 +20,10 @@ class ReviewsController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_review
     @review = Review.find(params[:id])
   end
 
-  # Only allow a list of trusted parameters through.
   def review_params
     params.require(:review).permit(:body)
   end
