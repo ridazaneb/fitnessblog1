68567be85ac32e82978ed21f92e13cfc141336e7
ridazaneb <157319768+ridazaneb@users.noreply.github.com>
2024-05-09 09:14:31 -0400

    Update goals_controller.rb

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,2 +1,12 @@
 class GoalsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
+
+  def index
+    if current_user
+      @goals = current_user.goals
+    else
+      @potential_goals = PotentialGoal.all
+    end
+  end
+
 end
