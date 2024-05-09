8943cb8c508262602e6a51336119eae9b6516f00
ridazaneb <157319768+ridazaneb@users.noreply.github.com>
2024-05-09 09:59:26 -0400

    Update goals_controller.rb

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,19 +1,6 @@
 
 class GoalsController < ApplicationController
-  def index
-    if current_user
-      @goals = current_user.goals
-      @potential_goals = []
-    else
-      @goals = []
-      @potential_goals = [
-        { name: "Lose Weight", description: "Achieve weight loss through diet and exercise." },
-        { name: "Build Muscle", description: "Increase muscle mass and strength through resistance training." },
-        { name: "Improve Endurance", description: "Enhance cardiovascular fitness and stamina." }
-        # Add more potential goals as needed
-      ]
-    end
-  end
+ 
   def new
     @goal = Goal.new
   end
