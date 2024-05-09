a6ed659a711763c26dc3c0094b3f99f3651a40ab
ridazaneb <157319768+ridazaneb@users.noreply.github.com>
2024-05-09 08:40:43 -0400

    Update reviews_controller.rb

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,24 @@
 class ReviewsController < ApplicationController
+  # GET /reviews/new
+  def new
+    @review = Review.new
+  end
+
+  # POST /reviews
+  def create
+    @review = current_user.reviews.build(review_params)
+    if @review.save
+      redirect_to root_path, notice: "Review was successfully created."
+    else
+      render :new
+    end
+  end
+
+  private
+
+  # Only allow a list of trusted parameters through.
+  def review_params
+    params.require(:review).permit(:body)
+  end
 end
+
