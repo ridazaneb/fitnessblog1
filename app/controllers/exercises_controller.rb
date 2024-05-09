d1601f2030b05202b56c88fdb09a9f06ac44c9e6
ridazaneb <157319768+ridazaneb@users.noreply.github.com>
2024-05-09 15:58:13 -0400

    Update exercises_controller.rb

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,5 +1,7 @@
 class ExercisesController < ApplicationController
   def index
     @exercises = Exercise.all
+      @exercise_records = current_user.exercise_records.includes(:exercise)
+
   end
 end
